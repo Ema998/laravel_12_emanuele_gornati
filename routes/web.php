934f90dc3a03465a15eccd34a2da782088c0c4c6
Ema998 <emanuele.gornati3@gmail.com>
2025-07-16 21:33:15 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
-use App\Http\Controllers\ArticoliController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\TagsController;
 
-Route::get('/', [ArticoliController::class, 'index'])->name('homepage');
-Route::post('articoli/articoli/store', [ArticoliController::class, 'store'])->name('articoli.store');
-Route::put('articoli/articoli.update/{articolo}', [ArticoliController::class, 'update'])->name('articoli.update');
-Route::delete('articoli/articoli.destroy/{articolo}', [ArticoliController::class, 'destroy'])->name('articoli.destroy');
-Route::get('articoli/articoli.create', [ArticoliController::class, 'create'])->name('articoli.create');
-Route::get('articoli/articoli.edit/{articolo}', [ArticoliController::class, 'edit'])->name('articoli.edit');
-Route::get('articoli/articoli.detail/{articolo}', [ArticoliController::class, 'show'])->name('articoli.detail');
+Route::get('/', [ArticleController::class, 'index'])->name('homepage');
+Route::post('articoli/articoli/store', [ArticleController::class, 'store'])->name('articoli.store');
+Route::put('articoli/articoli.update/{articolo}', [ArticleController::class, 'update'])->name('articoli.update');
+Route::delete('articoli/articoli.destroy/{articolo}', [ArticleController::class, 'destroy'])->name('articoli.destroy');
+Route::get('articoli/articoli.create', [ArticleController::class, 'create'])->name('articoli.create');
+Route::get('articoli/articoli.edit/{articolo}', [ArticleController::class, 'edit'])->name('articoli.edit');
+Route::get('articoli/articoli.detail/{articolo}', [ArticleController::class, 'show'])->name('articoli.detail');
 
 Route::get('tags/tags.create', [TagsController::class, 'create'])->name('tags.create');
 Route::post('tags/tags/store', [TagsController::class, 'store'])->name('tags.store');
